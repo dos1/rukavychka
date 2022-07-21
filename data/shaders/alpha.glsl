cb42ab2226907bbed098c1a4a94fceb40d168254
Sebastian Krzyszkowiak <dos@dosowisko.net>
2022-07-21 06:03:17 +0200

    data: shaders: Make sure colors in alpha shader don't go over 1.0

--- data/shaders/alpha.glsl
+++ data/shaders/alpha.glsl
@@ -5,10 +5,9 @@ precision highp float;
 uniform sampler2D al_tex;
 varying vec2 varying_texcoord;
 varying vec4 varying_color;
-varying vec4 varying_origpos;
 
 void main() {
 	vec4 color = texture2D(al_tex, varying_texcoord);
-	color *= (pow(1.0 - (color.r - 0.075), 0.75) * 0.04) + 0.075;
+	color *= (pow(1.0 - (color.r - 0.075), 0.75) * 0.025) + 0.075;
 	gl_FragColor = color * varying_color;
 }
--- data/vita/shaders/alpha.glsl
+++ data/vita/shaders/alpha.glsl
@@ -4,6 +4,6 @@ float4 main(
   float2 varying_texcoord : TEXCOORD0)
 {
 	float4 color = tex2D(al_tex, varying_texcoord);
-	color *= (pow(1.0 - (color.r - 0.075), 0.75) * 0.04) + 0.075;
-	return mul(color, varying_color);
+	color *= (pow(1.0 - (color.r - 0.075), 0.75) * 0.025) + 0.075;
+	return color * varying_color;
 }
